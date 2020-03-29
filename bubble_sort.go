15bb02d24da4b179a1406a4cdc24235d790ffd8f
Peterson Daronch de Bem <petersondaronch@gmail.com>
2020-03-29 23:17:18 +0000

    Add bubble sort

---
+++ bubble_sort.go
@@ -0,0 +1,19 @@
+package algorithms
+
+// BubbleSort sorts a collection of numbers
+func (a Algorithms) BubbleSort(numbers []int) {
+	for i := 0; i < len(numbers)-1; i++ {
+		swapped := false
+
+		for j := 0; j < len(numbers)-1-i; j++ {
+			if numbers[j] > numbers[j+1] {
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			break
+		}
+	}
+}
---
+++ test/bubble_sort_test.go
@@ -0,0 +1,53 @@
+package test
+
+import (
+	"algorithms"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	myAlgorithms := algorithms.Algorithms{}
+	cases := []struct {
+		unsortedNumbers []int
+		toSortNumbers   []int
+		want            []int
+	}{
+		{
+			unsortedNumbers: []int{2, 1, 3, 6, 5},
+			toSortNumbers:   []int{2, 1, 3, 6, 5},
+			want:            []int{1, 2, 3, 5, 6},
+		},
+		{
+			unsortedNumbers: []int{1, 2, 3, 5, 6},
+			toSortNumbers:   []int{1, 2, 3, 5, 6},
+			want:            []int{1, 2, 3, 5, 6},
+		},
+		{
+			unsortedNumbers: []int{},
+			toSortNumbers:   []int{},
+			want:            []int{},
+		},
+		{
+			unsortedNumbers: []int{-5, -2, 3, 5, -10},
+			toSortNumbers:   []int{-5, -2, 3, 5, -10},
+			want:            []int{-10, -5, -2, 3, 5},
+		},
+	}
+
+	for _, c := range cases {
+		myAlgorithms.BubbleSort(c.toSortNumbers)
+
+		for i := 0; i < len(c.want); i++ {
+			if c.want[i] != c.toSortNumbers[i] {
+				t.Errorf(
+					"BubbleSort(%v) == %v, want %v",
+					c.unsortedNumbers,
+					c.toSortNumbers,
+					c.want,
+				)
+
+				break
+			}
+		}
+	}
+}
